5cc16b0f5d21dc2b32fe9962f9b6f1d5a558f91d
galaxlabs <galaxylab2020@gmail.com>
2026-07-30 06:38:52 +0500

    fix: add controllers for configuration and wallet doctypes

---
+++ ftms/transport_system/doctype/agreement_template/agreement_template.py
@@ -0,0 +1,5 @@
+from frappe.model.document import Document
+
+
+class AgreementTemplate(Document):
+    pass
---
+++ ftms/transport_system/doctype/integration_settings/integration_settings.py
@@ -0,0 +1,5 @@
+from frappe.model.document import Document
+
+
+class IntegrationSettings(Document):
+    pass
