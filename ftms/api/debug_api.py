9860707c9bdd2545648b2e8c21dba23dae3fbff9
galaxlabs <galaxylab2020@gmail.com>
2026-08-07 20:09:32 +0500

    debug: header echo endpoint for diagnosing 417

---
+++ ftms/api/debug_api.py
@@ -0,0 +1,26 @@
+import frappe
+
+
+@frappe.whitelist(allow_guest=True, methods=["POST", "GET"])
+def debug_echo_headers():
+    """Diagnostic: echo request headers so we can see what the browser sends."""
+    import json
+
+    headers = {}
+    keys = [
+        "Expect", "Content-Type", "Authorization", "X-Requested-With",
+        "Origin", "Content-Length", "Referer", "User-Agent", "Accept",
+    ]
+    for key in keys:
+        val = frappe.get_request_header(key)
+        if val:
+            headers[key] = val[:200]
+    body = frappe.request.get_data(as_text=True)[:500]
+    payload = {
+        "headers": headers,
+        "body": body,
+        "method": frappe.request.method,
+        "protocol": frappe.request.environ.get("SERVER_PROTOCOL", ""),
+    }
+    frappe.log_error(json.dumps(payload), "DEBUG HEADERS")
+    return payload
